530482a8daf8d7de3221558685f035af5384fc15
JoPanta <jopantaleao@gmail.com>
2023-10-06 14:33:49 +0100

    canvas setting

---
+++ main.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+
+canvas = Canvas(width=200, height=200)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(105, 100, image=logo)
+canvas.grid(row=1, column=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
